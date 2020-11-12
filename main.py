a61bbf472f6c315baf1fd7ab2f786c55e8434187
Ainoa <ainoagm@gmail.com>
2020-11-12 22:12:40 +0100

    Reparto de cartas

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import barajaC
 
-palos = ['corazones','picas','diamantes','treboles']
-numeros = ['A','2','3','4','5','6','7','8', '9','J','Q','K']
+palos = ['o','c','e','b']
+numeros = ['A','2','3','4','5','6','7','S','C','R']
 
 miBaraja = barajaC.Baraja(palos, numeros)
 
 print (miBaraja.mazacote)
-miBaraja.barajar()
-print (miBaraja.mazacote)
+print (miBaraja.repartir(3,5))
+
